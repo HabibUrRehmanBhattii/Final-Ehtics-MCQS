672c1c753bafea36eac2afdf5c94f8bdaf72aef5
Habib-ur-Rehman Bhatti <HabibUrRehmanBhattii@users.noreply.github.com>
2026-02-07 12:37:45 -0500

    Update practice tests for LLQP Ethics and add LLQP Segregated Funds topic with new questions; increment cache version in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* PWA Service Worker for LLQP & WFG Exam Prep */
-const CACHE_VERSION = 'v1.0.1';
+const CACHE_VERSION = 'v1.0.3';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DATA_CACHE = `data-${CACHE_VERSION}`;
 
